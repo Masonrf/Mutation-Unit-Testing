f27fe40c66ac3efa108a346fc14f27fade06a7b0
Masonrf <masonrfleck@gmail.com>
2022-09-06 21:02:48 -0500

    add color and restructure loadSource()

--- mutation.py
+++ mutation.py
@@ -1,34 +1,36 @@
 import ast
+from colorama import *
+init()
 from typing import final
 
 class Mutation:
     def __init__(self, fileName: str):
         self.filename = fileName
-        self.srcStr = "{Invalid source string}"
-        try:
-            self.srcStr = self.__loadSource()
-        except:
-            print("An exception occured")
+        self.srcStr = Fore.YELLOW + Style.BRIGHT + "{Invalid source string}" + Style.RESET_ALL
+        self.__loadSource()
         
     # Loads Python source code from file. Returns that source code as a string
     def __loadSource(self):
         try:
             if not (self.filename.lower().endswith('.py')):
-                raise Exception('This program only supports python source in .py files')
-
-            srcFile = open(self.filename, "r")
-            srcString = srcFile.read()
+                raise Exception('This program only supports python source code in .py files')
+            with open(self.filename, "r") as srcFile:
+                self.srcString = srcFile.read()
 
         except FileNotFoundError:
-            print("File with name " + self.filename + " does not exist!")
+            print(Fore.RED + Style.BRIGHT + "\nFile with name " + self.filename + " does not exist!\n" + Style.RESET_ALL)
 
         except Exception as ex:
+            print(Fore.RED + Style.BRIGHT)
             print(ex)
+            print(Style.RESET_ALL)
 
-        finally:
-            srcFile.close()
-            return srcString
+        except:
+            print(Fore.RED + Style.BRIGHT + "\nAn exception occured when trying to read file " + self.filename + "\n" + Style.RESET_ALL)
+
+            
     
     # Print out the source string
     def printSrc(self):
-        print("[From " + self.filename + ":]\n" + self.srcStr)
+        print(Back.GREEN + "[From " + Style.BRIGHT + self.filename + Style.NORMAL + ":]" + Style.RESET_ALL)
+        print(self.srcStr + Style.RESET_ALL)
