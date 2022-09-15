fb6e4569484a941f8d30b6bfbe0caf9a7ccef882
Masonrf <masonrfleck@gmail.com>
2022-09-15 14:25:30 -0500

    export mutated tree as python source to a file

--- mutation.py
+++ mutation.py
@@ -1,24 +1,29 @@
 import ast
+from typing import Literal
 from colorama import *
 init()
 from alive_progress import alive_bar
+from enum import Enum
 
 class Mutation:
     def __init__(self, fileName: str):
         self.filename = fileName
         self.srcStr = ""
         print()
-        with alive_bar(2) as bar:
-            bar.title = 'Initializing'
+        with alive_bar(2, title='Initializing') as bar:
             print("Loading source from " + self.filename)
             self.__loadSource()
             bar()
 
             print("Parsing source into tree")
+            # It is possible to crash the Python interpreter with a sufficiently large/complex string due to stack depth limitations in Python’s AST compiler.
             self.tree = ast.parse(self.srcStr)
+            self.mutatedTree = self.tree
             bar()
             #analyze tree - look for pieces of code the unit test actually covers
-        
+
+
+
     # Loads Python source code from file. Returns that source code as a string
     def __loadSource(self):
         try:
@@ -28,18 +33,15 @@ class Mutation:
                 self.srcStr = srcFile.read()
 
         except FileNotFoundError:
-            print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " " + "File with name " + self.filename + " does not exist!" + Style.RESET_ALL)
-            raise Exception
+            print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " File with name " + self.filename + " does not exist!" + Style.RESET_ALL)
+            raise
 
         except Exception as ex:
+            print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " An exception of type " + type(ex).__name__ + " occured when trying to read file " + self.filename + ":" + Style.RESET_ALL)
             print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " " + str(ex) + Style.RESET_ALL)
-            raise Exception
+            raise
 
-        except:
-            print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " " + "An exception occured when trying to read file " + self.filename + Style.RESET_ALL)
-            raise Exception
 
-            
     
     # Print out the source string
     def printSrc(self):
@@ -50,7 +52,9 @@ class Mutation:
             print()
             print(Back.GREEN + "[From " + Style.BRIGHT + self.filename + Style.NORMAL + "]" + Style.RESET_ALL)
             print(self.srcStr + Style.RESET_ALL + "\n")
-    
+
+
+
     # Print out the parse tree
     def printTree(self):
         if self.srcStr == "":
@@ -61,6 +65,31 @@ class Mutation:
             print(Back.GREEN + "[Source file " + Style.BRIGHT + self.filename + Style.NORMAL + " as a tree]" + Style.RESET_ALL)
             print(ast.dump(self.tree, indent=2) + "\n")
     
+
+
+    # Converts the parse tree back into code
+    def exportMutatedTreeAsSource(self, destinationFilename):
+        try:
+            with alive_bar(2, title='Exporting Mutated Source') as bar:
+                print("Converting from tree to source")
+                mutatedSrc = ast.unparse(self.mutatedTree)
+                bar()
+
+                print("Writing to file " + destinationFilename)
+                with open(destinationFilename, "w") as destFile:
+                    # Some warnings about the unparse function from the library documentation:
+                    # Warning The produced code string will not necessarily be equal to the original code that generated the ast.AST object.
+                    # Trying to unparse a highly complex expression would result with RecursionError.
+                    destFile.write(mutatedSrc)
+                bar()
+                
+                
+
+        except Exception as ex:
+            print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " An exception of type " + type(ex).__name__ + " occured when trying to write file " + destinationFilename + ":" + Style.RESET_ALL)
+            print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " " + str(ex) + Style.RESET_ALL)
+            raise
+
     # mutate
     # "recompile"
     # execute unit tests & save
