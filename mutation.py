605fd2a29ed9983a2edbf6329f45b79f808b3fd7
Masonrf <masonrfleck@gmail.com>
2022-09-28 21:14:28 -0500

    mutate all binop complements

--- mutation.py
+++ mutation.py
@@ -1,25 +1,24 @@
 import ast
-from typing import Literal
 from colorama import *
 init()
 from alive_progress import alive_bar
-from enum import Enum
 import copy
+import traceback
 
 class Mutation:
     def __init__(self, fileName: str):
         self.filename = fileName
         self.srcStr = ""
         print()
-        with alive_bar(2, title='Initializing') as bar:
+        with alive_bar(2, title='Initializing') as initBar:
             print("Loading source from " + self.filename)
             self.__loadSource()
-            bar()
+            initBar()
 
             print("Parsing source into tree")
             # It is possible to crash the Python interpreter with a sufficiently large/complex string due to stack depth limitations in Python’s AST compiler.
             self.tree = ast.parse(self.srcStr)
-            bar()
+            initBar()
             #analyze tree - look for pieces of code the unit test actually covers
 
 
@@ -70,18 +69,19 @@ class Mutation:
     # Converts the parse tree back into code
     def __exportTreeAsSource(self, tree, destinationFilename):
         try:
-            with alive_bar(2, title='Exporting Mutated Source') as bar:
+            print("ahh")
+            with alive_bar(2, title='Exporting Mutated Source') as exportBar:
                 print("Converting from tree to source")
                 # Some warnings about the unparse function from the library documentation:
                 # Warning The produced code string will not necessarily be equal to the original code that generated the ast.AST object.
                 # Trying to unparse a highly complex expression would result with RecursionError.
                 src = ast.unparse(tree)
-                bar()
+                exportBar()
 
                 print("Writing to file " + destinationFilename)
                 with open(destinationFilename, "w") as destFile:
                     destFile.write(src)
-                bar()
+                exportBar()
                 
                 
 
@@ -93,29 +93,99 @@ class Mutation:
 
 
     # Types of mutations to be called with mutation.mutation_types.TYPE
-    class mutation_types(Enum):
+    class mutation_types():
         COMPLEMENT = 1
         RANDOM = 2
 
+    # Valid operators that can be used in the mutation
+    mutation_operators = {
+        "unaryOps": ("UAdd", "USub", "Not", "Invert"),
+        "binOps": ("Add", "Sub", "Mult", "Div", "FloorDiv", "Mod", "Pow", "LShift", "RShift", "BitOr", "BitXor", "BitAnd", "MatMult")
+    }
 
+
+    # Node transformer callback functions and info for visiting and modifying the AST
+    class __astNodeTransformerCallbacks(ast.NodeTransformer, mutation_types):
+        def __init__(self, operators: dict, mutationType):
+            self.operators = operators
+            self.mutationType = mutationType
+
+        def visit_UnaryOp(self, node):
+            return node
+
+        
+        def visit_BinOp(self, node):
+            try:
+                match self.mutationType:
+                    case self.COMPLEMENT:
+                        match node.op:
+                            case ast.Add():
+                                if "Sub" in self.operators["binOps"]:
+                                    node.op = ast.Sub()
+
+                            case ast.Sub():
+                                if "Add" in self.operators["binOps"]:
+                                    node.op = ast.Add()
+                        
+                            case ast.Mult():
+                                if "Div" in self.operators["binOps"]:
+                                    node.op = ast.Div()
+                        
+                            case ast.Div():
+                                if "Mult" in self.operators["binOps"]:
+                                    node.op = ast.Mult()
+                                
+                            case ast.LShift():
+                                if "Rshift" in self.operators["binOps"]:
+                                    node.op = ast.RShift()
+
+                            case ast.RShift():
+                                if "Lshift" in self.operators["binOps"]:
+                                    node.op = ast.LShift()
+
+                            
+                            case _:
+                                print("Operator of type ", type(node.op), " does not have a complementary operator.")
+
+                    case self.RANDOM:
+                        raise Exception('This mutation type is not yet implemented')
+
+                    case _:
+                        raise Exception('Unknown mutation type')
+
+                return node
+
+            except Exception:
+                raise
+
+
+        #def visit_Constant(self, node):
+        #    newNode = ast.Constant(100)
+        #    return ast.copy_location(newNode, node)
 
     # An abstraction to be able to call any type of mutation function from one function call
-    def mutate(self, mutation_type: Literal[1], iterations, numMutations):
+    def mutate(self, mutation_type, iterations, numMutations):
         try:
-            with alive_bar(iterations, title='Mutating') as bar:
+            with alive_bar(iterations, title='Mutating') as mutBar:
                 for i in range(iterations):
                     mutatedTree = copy.deepcopy(self.tree)
 
-                    if mutation_type == self.mutation_types.COMPLEMENT.value:
+                    if mutation_type == self.mutation_types.COMPLEMENT:
                         self.__mutateComplement(mutatedTree, numMutations)
 
-                    elif mutation_type == self.mutation_types.RANDOM.value:
+                    elif mutation_type == self.mutation_types.RANDOM:
                         pass
 
                     else:
                         raise Exception('Unknown mutation type')
                     
-                    bar()
+                    mutatedTree = ast.fix_missing_locations(mutatedTree)
+                    print(ast.unparse(mutatedTree))
+
+                    ## run unit test
+                    
+                    mutBar()
+
 
         except Exception as ex:
             print(Fore.WHITE + Back.RED + "[Error]" + Back.RESET + Style.BRIGHT + Fore.RED + " An exception of type " + type(ex).__name__ + " occured when trying to mutate:" + Style.RESET_ALL)
@@ -125,7 +195,11 @@ class Mutation:
     
 
     def __mutateComplement(self, mutatedTree, numMutations):
-        pass
+        try:
+            transformer = self.__astNodeTransformerCallbacks(self.mutation_operators, self.mutation_types.COMPLEMENT)
+            mutatedTree = transformer.visit(mutatedTree)
+        except Exception:
+            raise
 
 
 
